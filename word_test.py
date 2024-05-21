158aaa571ab7e8db6d57aac9d95ba3f3a2b9010a
burakkerem0 <burakkeremkoruk@gmail.com>
2024-05-22 00:50:34 +0300

    Update word_test.py

--- word_test.py
+++ word_test.py
@@ -1,6 +1,31 @@
 word_list = {
     "Associated" : "bağlantılı",
     "intention" : "amaç",
+    "wealthy" : "varlıklı",
+    "enhance" : "artırmak",      
+    "crucial" : "çok önemli",
+    "alter" : "değiştirmek", 
+    "assume": "varsaymak", 
+    "confidence": "güven",
+    "contribute": "katkı yapmak", 
+    "debate" : "tartışmak", 
+    "capable": "yetenekli",
+    "struggle" : "mücadele",
+    "generate": "oluşturmak",
+    "emphasize": "vurgulamak",
+    "satisfied" : "memnun",
+    "decant" : "dökmek",
+    "instance": "örnek",
+    "resolve" : "kararlılık",
+    "intention" : "amaç",
+    "tend" : "eğiliminde olmak",
+    "treat" : "davranmak",
+    "label" : "etiket",
+    "previously" : "daha önce",
+    "encourage": "cesaretlendirmek",
+    "indicate": "belirtmek",
+    "statue": "heykel",
+    "confident": "kendinden emin"
 
 }
 point = 0 
